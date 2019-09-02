e8a4383e2bd3cf177a4ecf9906148c89969e6223
Bikash <bikash@stridesolution.com.au>
2019-09-02 17:10:40 +1000

    added search input

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,10 @@ import SearchBar from "./SearchBar";
 
 const App = () => {
     return (
-        <div>
-           <SearchBar/>
+        <div className="ui container" style={{marginTop: '10px'}}>
+            <SearchBar/>
         </div>
     );
-
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+
+
+    onSearchInputChange(event) {
+        console.log(event.target.value);
+    }
+
     render() {
         return (
-            <form className="ui form">
-                <div className="field">
-                    <label>Image Search</label>
-                    <input placeholder="First Name"/>
-                </div>
-            </form>
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" placeholder="Search.." onChange={this.onSearchInputChange}/>
+                    </div>
+                </form>
+            </div>
         );
     }
 }
